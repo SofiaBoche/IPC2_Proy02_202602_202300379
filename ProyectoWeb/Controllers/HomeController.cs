c4bd728fb766e21c89a04b31a99b5de0be84129b
Sofia Boche <3676435880101@ingenieria.usac.edu.gt>
2026-09-25 16:38:30 -0600

    fix: Enlace de PDF agregado en HomeController

--- ProyectoWeb/Controllers/HomeController.cs
+++ ProyectoWeb/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace ProyectoWeb.Controllers
         {
             ViewBag.NombreEstudiante = "Mónica Sofia Boche Figueroa"; 
             ViewBag.Carnet = "202300379";                     
-            ViewBag.LinkDoc = "https://github.com/SofiaBoche/IPC2_Proy02_202602_202300379.git"; 
+            ViewBag.LinkDoc = "https://github.com/SofiaBoche/IPC2_Proy02_202602_202300379/blob/main/Documentacion/Ensayo_2_202300379.pdf"; 
 
             return View();
         }
